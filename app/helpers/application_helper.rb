9efd064b03adc9455bfa35be7ebbf46c5d9919ec
Erosanin <ashev08@gmail.com>
2020-04-07 17:53:28 +0200

    Add basic pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def active_class?(path)
+    'active' if request.path == path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   root 'main#index'
+
+  resources :cars, except: [:edit]
+
+  resources :reviews, only: [:create, :index, :destroy, :approve] do
+    member do
+      post '/approve' => "reviews#approve"
+    end
+  end
+
+  resources :bookings, only: [:create, :show, :index, :new] do
+    resources :documents, only: [:create]
+  end
+
+  resources :prices, only: [:index]
+  resources :contacts, only: [:index, :create]
+  resources :terms, only: [:index]
+  resources :faqs, only: [:index]
+  resources :abouts, only: [:index]
 end
