c7d8735043f79def3907532c1630a10cb9f85c20
Erosanin <ashev08@gmail.com>
2020-05-08 13:06:20 +0200

    try tg bot

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -16,15 +16,15 @@ class Booking < ApplicationRecord
     token = Rails.application.credentials.telegram_api_key
 
     Telegram::Bot::Client.run(token) do |bot|
-      bot.api.send_message(chat_id: 251685098, text: "Новая заявка от #{self.firstname} авто: #{self.car} тел: #{self.phone} с #{self.start_date} до #{self.end_date}")
-      # bot.listen do |message|
-      #   case message.text
-      #   when '/start'
-      #     bot.api.send_message(chat_id: 251685098, text: "Hello, #{message.from.first_name}")
-      #   when '/stop'
-      #     bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
-      #   end
-      # end
+      bot.api.send_message(chat_id: 251685098, text: "Новая заявка от #{self.name},#{self.phone},с #{self.start_date} до #{self.end_date} кузов: #{('Седан' if self.is_sedan) || ('Хетчбэк' if self.is_hatch) || ('Кроссовер' if self.is_cross) || ('Минивен' if self.is_minivan) || ('Универсал' if self.is_universal)}")
+      bot.listen do |message|
+        case message.text
+        when '/start'
+          bot.api.send_message(chat_id: 251685098, text: "Привет, #{message.from.first_name}")
+        when '/stop'
+          bot.api.send_message(chat_id: message.chat.id, text: "Пока, #{message.from.first_name}")
+        end
+      end
     end
   end
 end
