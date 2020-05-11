74ebe65aeecc3ae8749c4803f75a942967b264c1
Erosanin <ashev08@gmail.com>
2020-05-11 14:13:48 +0200

    try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,8 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  config.assets.digest = false
+
   config.active_storage.variable_content_types = %w(
     image/png
     image/gif
