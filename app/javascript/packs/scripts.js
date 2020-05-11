a687075a6018005df20696fcd953b49e32446574
Erosanin <ashev08@gmail.com>
2020-05-11 18:54:51 +0200

    Fix mobile menu open

--- app/javascript/packs/scripts.js
+++ app/javascript/packs/scripts.js
@@ -1,5 +1,5 @@
 // $(document).on('turbolinks:load', function(){
-    document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
     $(".block6 .col1 .pere .za").click(function(){
         if($(this).parent().hasClass("opened")) {
             $(this).parent().removeClass("opened");
@@ -33,23 +33,23 @@
         return false;
     });
 
-    $("#menu_tg1").click(function(){
-        $(".header .menu_top").slideUp();
-        $("#menu_tg2").removeClass("opened");
-        $(".header .addr, .header .phone, .header .vrem").slideToggle();
-        return false;
-    });
-    $("#menu_tg2").click(function(){
-        $(".header .addr, .header .phone, .header .vrem").slideUp();
-        $(".header .menu_top").slideToggle();
-        $("#menu_tg2").toggleClass("opened");
-        return false;
-    });
+    // $("#menu_tg1").click(function(){
+    //     $(".header .menu_top").slideUp();
+    //     $("#menu_tg2").removeClass("opened");
+    //     $(".header .addr, .header .phone, .header .vrem").slideToggle();
+    //     return false;
+    // });
+    // $("#menu_tg2").click(function(){
+    //     $(".header .addr, .header .phone, .header .vrem").slideUp();
+    //     $(".header .menu_top").slideToggle();
+    //     $("#menu_tg2").toggleClass("opened");
+    //     return false;
+    // });
 
-    $(".header .menu_top li a span").click(function(){
-        $(this).parent().parent().toggleClass("opened");
-        return false;
-    });
+    // $(".header .menu_top li a span").click(function(){
+    //     $(this).parent().parent().toggleClass("opened");
+    //     return false;
+    // });
 });
 
 
