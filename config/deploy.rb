aede10a47ebdb9f8713a26fa05143de3ea39182e
Erosanin <ashev08@gmail.com>
2020-05-12 18:24:06 +0200

    Add seeds to deploy rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,3 +24,15 @@ namespace :deploy do
     end
   end
 end
+namespace :deploy do
+  task :seed do
+    puts "\n=== Seeding Database ===\n"
+    on primary :db do
+      within current_path do
+        with rails_env: fetch(:stage) do
+          execute :rake, 'db:seed'
+        end
+      end
+    end
+  end
+end
