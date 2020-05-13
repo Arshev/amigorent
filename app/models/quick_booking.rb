2cd5f4b82b3760ebf74bfca724d87205d86da7be
Erosanin <ashev08@gmail.com>
2020-05-13 18:54:25 +0200

    try

--- app/models/quick_booking.rb
+++ app/models/quick_booking.rb
@@ -15,22 +15,7 @@ class QuickBooking < ApplicationRecord
     token = Rails.application.credentials.telegram_api_key
 
     Telegram::Bot::Client.run(token) do |bot|
-      # begin
-        bot.api.send_message(chat_id:430186294, text: "Новая заявка от #{self.name},#{self.phone},с #{self.start_date} до #{self.end_date} кузов: #{('Седан' if self.is_sedan) || ('Хетчбэк' if self.is_hatch) || ('Кроссовер' if self.is_cross) || ('Минивен' if self.is_minivan) || ('Универсал' if self.is_universal)}")
-      # rescue => exception
-      #   puts exception
-      # end
-      # bot.api.send_message(chat_id: 251685098, text: "Новая заявка от #{self.name},#{self.phone},с #{self.start_date} до #{self.end_date} кузов: #{('Седан' if self.is_sedan) || ('Хетчбэк' if self.is_hatch) || ('Кроссовер' if self.is_cross) || ('Минивен' if self.is_minivan) || ('Универсал' if self.is_universal)}")
-      
-      
-      # bot.listen do |message|
-      #   case message.text
-      #   when '/start'
-      #     bot.api.send_message(chat_id: 430186294, text: "Привет, #{message.from.first_name}")
-      #   when '/stop'
-      #     bot.api.send_message(chat_id: message.chat.id, text: "Пока, #{message.from.first_name}")
-      #   end
-      # end
+        bot.api.send_message(chat_id:430186294, text: "Новая заявка от #{self.name},#{self.phone},с #{self.start_date} до #{self.end_date} кузов: #{('Седан' if self.is_sedan) || ('Хетчбэк' if self.is_hatch) || ('Кроссовер' if self.is_cross) || ('Минивен' if self.is_minivan) || ('Универсал' if self.is_universal)}") 
     end
   end
   
