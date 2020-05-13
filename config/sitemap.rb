f0b774146f6ebcd5066a549d5161181eb3ac09bc
Erosanin <ashev08@gmail.com>
2020-05-13 13:55:21 +0200

    Counters

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "https://amigorent.ru"
+SitemapGenerator::Sitemap.default_host = 'https://amigorent.ru'
 SitemapGenerator::Sitemap.sitemaps_path = 'public/'
 SitemapGenerator::Sitemap.compress = false
 
