0ebbd16373778f4f5b64f6c66b95262e6695fe40
Erosanin <ashev08@gmail.com>
2020-05-14 11:17:58 +0200

    fix tg

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -19,7 +19,7 @@ class Booking < ApplicationRecord
     token = Rails.application.credentials.telegram_api_key
 
     Telegram::Bot::Client.run(token) do |bot|
-        bot.api.send_message(chat_id:430186294, text: "Новая заявка от #{self.name},#{self.phone},с #{self.start_date} до #{self.end_date} кузов: #{('Седан' if self.is_sedan) || ('Хетчбэк' if self.is_hatch) || ('Кроссовер' if self.is_cross) || ('Минивен' if self.is_minivan) || ('Универсал' if self.is_universal)}") 
+        bot.api.send_message(chat_id:430186294, text: "#{self.firstname} авто: #{self.car} тел: #{self.phone} с #{self.start_date} до #{self.end_date}") 
     end
   end
 end
