9e20b0f2e980be4ed5f49d1aeca2c3ef66cadc1b
Erosanin <ashev08@gmail.com>
2020-06-05 16:12:40 +0200

    Sitemap and robots en

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -11,6 +11,13 @@ SitemapGenerator::Sitemap.create do
   add contacts_path, :priority => 0.3, :changefreq => 'weekly'
   add bookings_path, :priority => 0.3, :changefreq => 'weekly'
 
+  add cars_en_path, :priority => 0.3, :changefreq => 'daily'
+  add abouts_en_path, :priority => 0.3, :changefreq => 'weekly'
+  add faqs_en_path, :priority => 0.3, :changefreq => 'weekly'
+  add prices_en_path, :priority => 0.3, :changefreq => 'daily'
+  add contacts_en_path, :priority => 0.3, :changefreq => 'weekly'
+  add bookings_en_path, :priority => 0.3, :changefreq => 'weekly'
+
   add arenda_avto_aeroport_hrabrovo_path, :priority => 0.5, :changefreq => 'weekly'
   add arenda_avto_zelenogradsk_path, :priority => 0.5, :changefreq => 'weekly'
   add arenda_avto_svetlogorsk_path, :priority => 0.5, :changefreq => 'weekly'
@@ -18,6 +25,14 @@ SitemapGenerator::Sitemap.create do
   add arenda_avto_baltyisk_path, :priority => 0.5, :changefreq => 'weekly'
   add arenda_avto_pionerskyi_path, :priority => 0.5, :changefreq => 'weekly'
   add arenda_avto_chernyahovsk_path, :priority => 0.5, :changefreq => 'weekly'
+
+  add arenda_avto_aeroport_hrabrovo_en_path, :priority => 0.3, :changefreq => 'weekly'
+  add arenda_avto_zelenogradsk_en_path, :priority => 0.3, :changefreq => 'weekly'
+  add arenda_avto_svetlogorsk_en_path, :priority => 0.3, :changefreq => 'weekly'
+  add arenda_avto_yantarnyi_en_path, :priority => 0.3, :changefreq => 'weekly'
+  add arenda_avto_baltyisk_en_path, :priority => 0.3, :changefreq => 'weekly'
+  add arenda_avto_pionerskyi_en_path, :priority => 0.3, :changefreq => 'weekly'
+  add arenda_avto_chernyahovsk_en_path, :priority => 0.3, :changefreq => 'weekly'
 
   add arenda_avto_s_voditelem_path, :priority => 0.5, :changefreq => 'weekly'
   add car_sharing_path, :priority => 0.5, :changefreq => 'weekly'
@@ -28,8 +43,18 @@ SitemapGenerator::Sitemap.create do
   add transportnyi_autsorsing_path, :priority => 0.5, :changefreq => 'weekly'
   add full_insurance_path, :priority => 0.5, :changefreq => 'weekly'
 
+  add arenda_avto_s_voditelem_en_path, :priority => 0.3, :changefreq => 'weekly'
+  add car_sharing_en_path, :priority => 0.3, :changefreq => 'weekly'
+  add casko_en_path, :priority => 0.3, :changefreq => 'weekly'
+  add corporativnym_clientam_en_path, :priority => 0.3, :changefreq => 'weekly'
+  add sales_system_en_path, :priority => 0.3, :changefreq => 'weekly'
+  add payment_methods_en_path, :priority => 0.3, :changefreq => 'weekly'
+  add transportnyi_autsorsing_en_path, :priority => 0.3, :changefreq => 'weekly'
+  add full_insurance_en_path, :priority => 0.3, :changefreq => 'weekly'
+
   Car.find_each do |car|
     add car_path(car), :lastmod => car.updated_at
+    add car_en_path(car), :lastmod => car.updated_at
   end
 
   Article.find_each do |article|
