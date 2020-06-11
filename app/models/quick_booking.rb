84eda40317734969b738fa9e1d3d84f0f0bd9211
Erosanin <ashev08@gmail.com>
2020-06-11 11:55:10 +0200

    fix Минивэн

--- app/models/quick_booking.rb
+++ app/models/quick_booking.rb
@@ -7,7 +7,7 @@ class QuickBooking < ApplicationRecord
   validates :end_date, presence: true
   
   def send_sms
-    message = MainsmsApi::Message.new(message: "Новая заявка от #{self.name},#{self.phone},с #{self.start_date} до #{self.end_date} кузов: #{('Седан' if self.is_sedan) || ('Хетчбэк' if self.is_hatch) || ('Кроссовер' if self.is_cross) || ('Минивен' if self.is_minivan) || ('Универсал' if self.is_universal)}", recipients: ['79217101615'])
+    message = MainsmsApi::Message.new(message: "Новая заявка от #{self.name},#{self.phone},с #{self.start_date} до #{self.end_date} кузов: #{('Седан' if self.is_sedan) || ('Хетчбэк' if self.is_hatch) || ('Кроссовер' if self.is_cross) || ('Минивэн' if self.is_minivan) || ('Универсал' if self.is_universal)}", recipients: ['79217101615'])
     response = message.deliver
   end
 
@@ -15,7 +15,7 @@ class QuickBooking < ApplicationRecord
     token = Rails.application.credentials.telegram_api_key
 
     Telegram::Bot::Client.run(token) do |bot|
-        bot.api.send_message(chat_id:430186294, text: "Новая заявка от #{self.name},#{self.phone},с #{self.start_date} до #{self.end_date} кузов: #{('Седан' if self.is_sedan) || ('Хетчбэк' if self.is_hatch) || ('Кроссовер' if self.is_cross) || ('Минивен' if self.is_minivan) || ('Универсал' if self.is_universal)}") 
+        bot.api.send_message(chat_id:430186294, text: "Новая заявка от #{self.name},#{self.phone},с #{self.start_date} до #{self.end_date} кузов: #{('Седан' if self.is_sedan) || ('Хетчбэк' if self.is_hatch) || ('Кроссовер' if self.is_cross) || ('Минивэн' if self.is_minivan) || ('Универсал' if self.is_universal)}") 
     end
   end
   
