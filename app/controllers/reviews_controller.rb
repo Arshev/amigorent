87053fa794aea48c9bdcff19f0c54a44a9458c75
Erosanin <ashev08@gmail.com>
2020-06-15 17:32:58 +0200

    New captcha

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,24 +1,26 @@
 class ReviewsController < ApplicationController
 
   before_action :set_text, only: [:index]
+  invisible_captcha only: [:create], honeypot: :subtitle
 
   def create
     @review = Review.new(review_params)
-    recaptcha_valid = verify_recaptcha(model:@review)
-    if recaptcha_valid
+    # recaptcha_valid = verify_recaptcha(model:@review)
+    # if recaptcha_valid
       if @review.save
         ReviewMailer.with(review: @review).review_email.deliver_later
         redirect_back(fallback_location: request.referer, notice: "Отзыв успешно создан! Он будет проверен на предмет спама и размещен.")
       else
         redirect_back(fallback_location: request.referer, alert: "Что то пошло не так!")
       end
-    else
-      redirect_back(fallback_location: request.referer, alert: "Вы не прошли проверку на ботов!")
-    end
+    # else
+    #   redirect_back(fallback_location: request.referer, alert: "Вы не прошли проверку на ботов!")
+    # end
   end
 
   def index
     @reviews = Review.paginate(page: params[:page])
+    @review = Review.new
   end
 
   def destroy
@@ -40,6 +42,7 @@ class ReviewsController < ApplicationController
     def review_params
       params.require(:review).permit(:text, :star, :active, :name, :email)
     end
+
     def set_text
       @text = Text.first
     end
