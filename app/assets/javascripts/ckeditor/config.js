92043499ed611ca3bbeaed7c79c18e7eb22b3d8f
Erosanin <ashev08@gmail.com>
2020-06-18 10:53:51 +0200

    try

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -2,4 +2,5 @@ CKEDITOR.editorConfig = function (config) {
   config.autoParagraph = false;
   config.protectedSource.push(/<(style)[^>]*>.*<\/style>/ig);
   config.allowedContent = true; /* all tags */
+  config.entities = false;
 }
