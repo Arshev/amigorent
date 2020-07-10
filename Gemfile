c933ee86f1df1d3be4489366bd8e87a289d1ac58
Erosanin <ashev08@gmail.com>
2020-07-10 16:18:39 +0200

    Ransack search in bookings

--- Gemfile
+++ Gemfile
@@ -49,6 +49,7 @@ gem 'rails-i18n', '~> 6.0.0'
 gem 'route_translator', '~> 8.0.0'
 gem 'invisible_captcha', '~> 1.0.1'
 gem 'dalli', '~> 2.7.10'
+gem 'ransack'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -15,7 +15,17 @@ class AdminsController < ApplicationController
     @cars = Car.all
   end
   def bookings
-    @bookings = Booking.paginate(page: params[:page])
+    # @bookings = Booking.paginate(page: params[:page])
+    @q = Booking.ransack(params[:q])
+    params[:q][:start_date_lteq] =  params[:q][:start_date_lteq].to_datetime.end_of_day.strftime("%d-%m-%Y %H:%M") if !(params[:q] && params[:q][:start_date_lteq]).blank?
+    puts params[:q][:start_date_lteq] if !(params[:q] && params[:q][:start_date_lteq]).blank?
+    params[:q][:start_date_gteq] =  params[:q][:start_date_gteq].to_datetime.end_of_day.strftime("%d-%m-%Y %H:%M") if !(params[:q] && params[:q][:start_date_gteq]).blank?
+    puts params[:q][:start_date_gteq] if !(params[:q] && params[:q][:start_date_gteq]).blank?
+    params[:q][:end_date_lteq] =  params[:q][:end_date_lteq].to_datetime.end_of_day.strftime("%d-%m-%Y %H:%M") if !(params[:q] && params[:q][:end_date_lteq]).blank?
+    puts params[:q][:end_date_lteq] if !(params[:q] && params[:q][:end_date_lteq]).blank?
+    params[:q][:end_date_gteq] =  params[:q][:end_date_gteq].to_datetime.end_of_day.strftime("%d-%m-%Y %H:%M") if !(params[:q] && params[:q][:end_date_gteq]).blank?
+    puts params[:q][:end_date_gteq] if !(params[:q] && params[:q][:end_date_gteq]).blank?
+    @bookings = @q.result.paginate(page: params[:page])
   end
 
   def edit_booking
