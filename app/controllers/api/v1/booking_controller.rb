8d8d5f4b4b7cd69c2666428161831934840c4ec9
Erosanin <ashev08@gmail.com>
2020-08-07 11:30:20 +0200

    try

--- app/controllers/api/v1/booking_controller.rb
+++ app/controllers/api/v1/booking_controller.rb
@@ -48,12 +48,14 @@ class Api::V1::BookingController < ApiController
                     #create client and booking
                     url_create_client = 'https://api.rentprog.ru/api/v1/create_client'
                     resp_create_client = Faraday.post(url_create_client) do |req|
+                        logger.info req
                         req.body = { clients: {name: "#{@booking.firstname.capitalize}", lastname: "#{@booking.lastname.capitalize}", middlename: "#{@booking.middlename.capitalize}", phone: "#{@booking.phone}", email: "#{@booking.email}" } }.to_json
                         req.headers['Content-Type'] = 'application/json'
                     end
                     resp_create_client_resp = JSON.parse resp_create_client.body.force_encoding("UTF-8")
                     url_create_booking = 'https://api.rentprog.ru/api/v1/create_booking'
                     resp_create_booking = Faraday.post(url_create_booking) do |req|
+                        logger.info req
                         req.body = { bookings: {company_id: 2, active: "false", client_id: "#{resp_create_client_resp.body.id}", first_name: "#{@booking.firstname.capitalize}", last_name: "#{@booking.lastname.capitalize}", middle_name: "#{@booking.middlename.capitalize}", car_name: "#{@booking.car}", start_date: "#{@booking.start_date}", end_date: "#{@booking.end_date}", additional_hours: "#{@booking.additional_hours}", location_start: "#{@booking.location_start}", location_end: "#{@booking.location_end}", chair: "#{@booking.baby_chair}", navigator: "#{@booking.navigator}", days: "#{@booking.days}", total: "#{@booking.total}", deposit: "#{@booking.deposit}", price: "#{@booking.price}" } }.to_json
                         req.headers['Content-Type'] = 'application/json'
                     end
