08206838e5d14e66aef3d91c6b2b66ef32cb22eb
Erosanin <ashev08@gmail.com>
2020-08-07 19:29:12 +0200

    Add purge css

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+let environment = {
   plugins: [
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
@@ -7,6 +7,21 @@ module.exports = {
         flexbox: 'no-2009'
       },
       stage: 3
-    })
+    }),
   ]
+}
+
+// Only run PurgeCSS in production (you can also add staging here)
+if (process.env.RAILS_ENV === "production") {
+  environment.plugins.push(
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        './app/**/*.html.erb',
+        './app/helpers/**/*.rb',
+        './app/javascript/**/*.js',
+        './app/javascript/**/*.vue'
+      ],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
+  )
 }
