af8855c9769eb8513d313e8dc04ee0b36842d2f1
Artem Shevchenko <ashev08@gmail.com>
2021-09-16 20:07:55 +0200

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,12 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.16.0"
+lock '~> 3.16.0'
 
-require "capistrano-db-tasks"
+require 'capistrano-db-tasks'
 
-set :application, "amigorent"
-set :repo_url, "git@github.com:Arshev/amigorent.git"
+set :application, 'amigorent'
+set :repo_url, 'git@github.com:Arshev/amigorent.git'
 set :deploy_to, "/home/deployer/#{fetch :application}"
+set :branch, ENV['BRANCH'] if ENV['BRANCH']
 
 set :use_sudo, true #try fix Cleaning stale instance directory /tmp/passenger.fvb84h1 01     Warning: Operation not permitted @ rb_file_chown
 
@@ -16,27 +17,41 @@ set :db_local_clean, false
 set :db_remote_clean, true
 
 # configure location where the dump file should be created
-set :db_dump_dir, -> { File.join(current_path, "db") }
+set :db_dump_dir, -> { File.join(current_path, 'db') }
 
 # if you prefer bzip2/unbzip2 instead of gzip
 set :compressor, :bzip2
 # set :npm_flags, '--production' # full log
 
-require "whenever/capistrano"
+require 'whenever/capistrano'
 
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", ".bundle", "public/system", "public/uploads", "public/storage", "storage", "node_modules", "public/packs"
+append :linked_dirs,
+       'log',
+       'tmp/pids',
+       'tmp/cache',
+       'tmp/sockets',
+       'vendor/bundle',
+       '.bundle',
+       'public/system',
+       'public/uploads',
+       'public/storage',
+       'storage',
+       'node_modules',
+       'public/packs'
 
-append :linked_files, "config/database.yml", "public/sitemap.xml"
+append :linked_files, 'config/database.yml', 'public/sitemap.xml'
 
 set :keep_releases, 5
 
-before "deploy:assets:precompile", "deploy:yarn_install"
+before 'deploy:assets:precompile', 'deploy:yarn_install'
 namespace :deploy do
-  desc "Run rake yarn install"
+  desc 'Run rake yarn install'
   task :yarn_install do
     on roles(:web) do
       within release_path do
-        execute("cd #{release_path} && yarn install --silent --no-progress --no-audit --no-optional")
+        execute(
+          "cd #{release_path} && yarn install --silent --no-progress --no-audit --no-optional",
+        )
       end
     end
   end
@@ -47,7 +62,7 @@ namespace :deploy do
     on primary :db do
       within current_path do
         with rails_env: fetch(:stage) do
-          execute :rake, "db:seed"
+          execute :rake, 'db:seed'
         end
       end
     end
